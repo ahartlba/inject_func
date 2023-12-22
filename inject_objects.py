b331a88a51b0c79966f9f0466a87c75115e4b097
Andreas Hartl-Bachinger <88311811+ahartlba@users.noreply.github.com>
2023-12-22 11:49:50 +0100

    rewrite decorator to object
    convert decorator from function to class

--- inject_objects.py
+++ inject_objects.py
@@ -1,17 +1,18 @@
 from functools import wraps
 
+class inject_objects:
+    def __init__(self, cleanup: bool = True, **objects):
+        self._cleanup = cleanup
+        self._objects = objects
 
-def inject_objects(**objects):
-    def decorator(func):
+    def __call__(self, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            for k, v in objects.items():
+            for k, v in self._objects.items():
                 func.__globals__[k] = v
             res = func(*args, **kwargs)
-            for k, v in objects.items():
-                del func.__globals__[k]
+            if self._cleanup:
+                for k, v in self._objects.items():
+                    del func.__globals__[k]
             return res
-
         return wrapper
-
-    return decorator
