0609ebce170b8564de02c19a8c5476fc3bde5307
Andreas Hartl-Bachinger <88311811+ahartlba@users.noreply.github.com>
2023-12-22 10:16:21 +0100

    Update inject_data.py

--- inject_data.py
+++ inject_data.py
@@ -1,14 +1,17 @@
 from functools import wraps
 
 
-def inject_data(func):
-    @wraps(func)
-    def wrapper(**kwargs):
-        for k, v in kwargs.items():
-            func.__globals__[k] = v
-        res = func()
-        for k, v in kwargs.items():
-            del func.__globals__[k]
-        return res
+def inject_objects(**objects):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for k, v in objects.items():
+                func.__globals__[k] = v
+            res = func(*args, **kwargs)
+            for k, v in objects.items():
+                del func.__globals__[k]
+            return res
 
-    return wrapper
+        return wrapper
+
+    return decorator
